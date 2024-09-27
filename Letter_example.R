d1e60e4335a5551e154847e26e1b6dbadcd0ebc2
Benjamin Snyder <b.snyder@tamu.edu>
2024-09-27 17:27:13 -0400

    delete output print from Letter_example.R

--- Letter_example.R
+++ Letter_example.R
@@ -5,12 +5,10 @@
 letter_train <- read.table("Data/letter-train.txt", header = F, colClasses = "numeric")
 Y <- letter_train[, 1]
 X <- as.matrix(letter_train[, -1])
-
 # Testing data
 letter_test <- read.table("Data/letter-test.txt", header = F, colClasses = "numeric")
 Yt <- letter_test[, 1]
 Xt <- as.matrix(letter_test[, -1])
-
 # [ToDo] Make sure to add column for an intercept to X and Xt
 
 vec_1 <- rep(1, dim(X)[1])
@@ -25,10 +23,9 @@ source("FunctionsLR.R")
 # [ToDo] Try the algorithm LRMultiClass with lambda = 1 and 50 iterations. Call the resulting object out, i.e. out <- LRMultiClass(...)
 
 out <- LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
-out
 
 # The code below will draw pictures of objective function, as well as train/test error over the iterations
-plot(out$objective, type = 'o')
+plot(out$objective, type = 'o', main = "Objective Funciton", xlab = "Iteration", ylab = "Objective Value")
 plot(out$error_train, type = 'o')
 plot(out$error_test, type = 'o')
 
