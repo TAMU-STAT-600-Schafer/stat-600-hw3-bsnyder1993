35828a283e8c143f8da71c9c0bf5a05269102156
Benjamin Snyder <b.snyder@tamu.edu>
2024-10-28 15:02:44 -0500

    upload this shit to github

--- FunctionsLR.R
+++ FunctionsLR.R
@@ -9,6 +9,11 @@
 # eta - learning rate, default value is 0.1
 # lambda - ridge parameter, default value is 1
 # beta_init - (optional) initial starting values of beta for the algorithm, should be p x K matrix
+library(Rcpp)
+library(RcppArmadillo)
+library(microbenchmark)
+library(testthat)
+sourceCpp("LMR_C.cpp")
 
 function_beta <- function(i, Pk_mat, y){
   indicator_sum <- sum(log(Pk_mat[y == (i-1), i]))
@@ -20,7 +25,7 @@ function_beta <- function(i, Pk_mat, y){
 # error_train - (numIter + 1) length vector of training error % at each iteration (+ starting value)
 # error_test - (numIter + 1) length vector of testing error % at each iteration (+ starting value)
 # objective - (numIter + 1) length vector of objective values of the function that we are minimizing at each iteration (+ starting value)
-LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = .5, beta_init = NULL){
+LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL){
   ## Check the supplied parameters as described. You can assume that X, Xt are matrices; y, yt are vectors; and numIter, eta, lambda are scalars. You can assume that beta_init is either NULL (default) or a matrix.
   ###################################
   
@@ -151,4 +156,56 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = .5, bet
   # error_test - (numIter + 1) length vector of testing error % at each iteration (+ starting value)
   # objective - (numIter + 1) length vector of objective values of the function that we are minimizing at each iteration (+ starting value)
   return(list(beta = beta, error_train = error_train, error_test = error_test, objective =  objective))
+}
+
+LRMultiClass_2 <- function(X, y, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL){
+  
+  # Compatibility checks from HW3 and initialization of beta_init
+  if (!all(X[, 1] == 1)) {
+    stop("The first column of X must be 1s.")
+  }
+  
+  # Check for compatibility of dimensions between X and Y
+  if (nrow(X) != length(y)) {
+    stop("X number of rows doesn't match length of Y")
+  }
+  
+  # Check eta is positive
+  if (eta <= 0) {
+    stop("Eta is not positive")
+  }
+  
+  # Check lambda is non-negative
+  if (lambda < 0) {
+    stop("Lambda is negative")
+  }
+  
+  n <- nrow(X)
+  p <- ncol(X)
+  K <- length(unique(y))
+  
+  
+  
+  # Check whether beta_init is NULL. If NULL, initialize beta with p x K matrix of zeroes. If not NULL, check for compatibility of dimensions with what has been already supplied.
+  if (is.null(beta_init)) {
+    beta_init <- matrix(0, nrow = p, ncol = K)
+  } else {
+    if (nrow(beta_init) != ncol(X)) {
+      stop("Length of beta doesn't match number of columns of X")
+    }
+    if (ncol(beta_init) != K) {
+      stop("Numbers of categories of beta and Y don't match")
+    }
+  }
+  
+  #sourceCpp("src/LRMultiClass.cpp")
+  # Call C++ LRMultiClass_c function to implement the algorithm
+  #print("After source")
+  #print(dim(X))
+  #print(length(y))
+  #print(dim(beta_init))
+  out = LRMultiClass_c(X, y, beta_init, numIter, eta, lambda)
+  #print("After c code")
+  # Return the class assignments
+  return(out)
 }
--- Tests.R
+++ Tests.R
@@ -1,6 +1,3 @@
-# This is a script to save your own tests for the function
-source("FunctionsLR.R")
-
 # Function that implements multi-class logistic regression.
 #############################################################
 # Description of supplied parameters:
@@ -11,7 +8,7 @@ source("FunctionsLR.R")
 # numIter - number of FIXED iterations of the algorithm, default value is 50
 # eta - learning rate, default value is 0.1
 # lambda - ridge parameter, default value is 1
-# beta_init - (optional) initial starting values of beta for the algorithm, should be p x K matrix 
+# beta_init - (optional) initial starting values of beta for the algorithm, should be p x K matrix
 
 function_beta <- function(i, Pk_mat, y){
   indicator_sum <- sum(log(Pk_mat[y == (i-1), i]))
@@ -23,63 +20,128 @@ function_beta <- function(i, Pk_mat, y){
 # error_train - (numIter + 1) length vector of training error % at each iteration (+ starting value)
 # error_test - (numIter + 1) length vector of testing error % at each iteration (+ starting value)
 # objective - (numIter + 1) length vector of objective values of the function that we are minimizing at each iteration (+ starting value)
-LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL){
+LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = .5, beta_init = NULL){
   ## Check the supplied parameters as described. You can assume that X, Xt are matrices; y, yt are vectors; and numIter, eta, lambda are scalars. You can assume that beta_init is either NULL (default) or a matrix.
   ###################################
+  
+  n1 <- dim(X)[1]
+  n2 <- dim(Xt)[1]
+  p <- dim(X)[2]
+  K <- length(unique(y))
+  
+  X_trans <- t(X)
+  
+  objective <- rep(0, numIter+1)
+  error_train <- rep(0, numIter+1)
+  error_test <- rep(0, numIter+1)
+  
   # Check that the first column of X and Xt are 1s, if not - display appropriate message and stop execution.
+  
+  if(sum(as.numeric(X[ ,1] == 1)) == n1 && sum(as.numeric(Xt[ ,1] == 1)) == n2){}
+  else{
+    stop("Error: First column of X and Xt must be 1's.")
+  }
   
   # Check for compatibility of dimensions between X and Y
   
+  if(length(y) == n1){}
+  else{
+    stop("Error: incompatible dimensions with X and Y.")
+  }
+  
   # Check for compatibility of dimensions between Xt and Yt
+  
+  if(length(yt) == n2){}
+  else{
+    stop("Error: incompatible dimensions with Xt and Yt.")
+  }
   
   # Check for compatibility of dimensions between X and Xt
   
+  if(dim(X)[2] == dim(Xt)[2]){}
+  else{
+    stop("Error: incompatible dimensions with X and Xt")
+  }
+  
   # Check eta is positive
+  
+  if(eta <= 0){
+    stop("Error: eta must be positive.")
+  }
   
   # Check lambda is non-negative
   
-  # Check whether beta_init is NULL. If NULL, initialize beta with p x K matrix of zeroes. If not NULL, check for compatibility of dimensions with what has been already supplied.
+  if(lambda < 0){
+    stop("Error: lambda must be non-negative")
+  }
   
-  beta <- beta_init
+  # Check whether beta_init is NULL. If NULL, initialize beta with p x K matrix of zeroes. If not NULL, check for compatibility of dimensions with what has been already supplied.
+  if(is.null(beta_init)){
+    beta <- matrix(0, nrow = dim(X)[2], ncol = K)
+  }
+  else{
+    beta <- beta_init
+  }
   
   ## Calculate corresponding pk, objective value f(beta_init), training error and testing error given the starting point beta_init
   ##########################################################################
-
+  
   X_beta <- X %*% beta
   exp_X_beta <- exp(X_beta)
-  Pk_mat <- (1/rowSums(exp_X_beta)) * exp_X_beta
   
-  sum_log <- sapply(1:k, \(i){function_beta(i, Pk_mat, y)})
-  f_beta <- sum(beta * beta) * (lamda / 2) - sum(sum_log)
-
+  Pk_mat <- exp_X_beta/rowSums(exp_X_beta)
+  
+  sum_log <- sapply(1:K, \(i){function_beta(i, Pk_mat, y)})
+  objective[1] <- sum(beta * beta) * (lambda / 2) - sum(sum_log)
+  
+  y_guess <- max.col(Pk_mat)-1
+  error_train[1] <- (1 - sum(as.numeric(y == y_guess))/n1) * 100
+  
+  Xt_beta <- Xt %*% beta
+  exp_Xt_beta <- exp(Xt_beta)
+  
+  Pk_test <- exp_Xt_beta/rowSums(exp_Xt_beta)
+  yt_guess <- max.col(Pk_test)-1
+  error_test[1] <- (1 - sum(as.numeric(yt == yt_guess))/n2) * 100
   
   ## Newton's method cycle - implement the update EXACTLY numIter iterations
   ##########################################################################
   
   # Within one iteration: perform the update, calculate updated objective function and training/testing errors in %
   
-  for(i in 1:nIter){
+  for(i in 1:numIter){
     
     for(k in 1:K){
       
       w <- Pk_mat[ , k] * (1-Pk_mat[ , k])
-      hessian <- tcrossprod(X, w*X)
+      hessian <- X_trans %*% (w * X)
       diag(hessian) <- diag(hessian) + lambda
-      hessian_inv <- chol2inv(chol(hessian))
+      #hessian_inv <- chol2inv(chol(hessian))
+      hessian_inv <- solve(hessian)
       
-      jacobian <- tcrossprod(X, Pk_mat[ , k] - as.numeric(y == (k-1))) + lambda * beta[ , k]
+      prob_vec <- Pk_mat[ , k] - as.numeric(y == (k-1))
+      jacobian <- X_trans %*% prob_vec + lambda * beta[ , k]
       
-      beta[ , k] <- beta[ , k] + eta * hessian_inv %*% jacobian
+      beta[ , k] <- beta[ , k] - eta * hessian_inv %*% jacobian
       
     }
     
     X_beta <- X %*% beta
     exp_X_beta <- exp(X_beta)
-    Pk_mat <- (1/rowSums(exp_X_beta)) * exp_X_beta
+    Pk_mat <- exp_X_beta/rowSums(exp_X_beta)
     
-    sum_log <- sapply(1:k, \(i){function_beta(i, Pk_mat, y)})
-    f_beta <- sum(beta * beta) * (lamda / 2) - sum(sum_log)
+    y_guess <- max.col(Pk_mat)-1
+    error_train[i+1] <- (1 - sum(as.numeric(y == y_guess))/n1) * 100
     
+    sum_log <- sapply(1:K, \(i){function_beta(i, Pk_mat, y)})
+    objective[i+1] <- sum(beta * beta) * (lambda / 2) - sum(sum_log)
+    
+    Xt_beta <- Xt %*% beta
+    exp_Xt_beta <- exp(Xt_beta)
+    Pk_test <- exp_Xt_beta/rowSums(exp_Xt_beta)
+    
+    yt_guess <- max.col(Pk_test)-1
+    error_test[i+1] <- (1 - sum(as.numeric(yt == yt_guess))/n2) * 100
   }
   
   ## Return output
@@ -90,26 +152,47 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta
   # objective - (numIter + 1) length vector of objective values of the function that we are minimizing at each iteration (+ starting value)
   return(list(beta = beta, error_train = error_train, error_test = error_test, objective =  objective))
 }
+# Application of multi-class logistic to letters data
+# Load the letter data
+#########################
+# Training data
+letter_train <- read.table("Data/letter-train.txt", header = F, colClasses = "numeric")
+Y <- letter_train[, 1]
+X <- as.matrix(letter_train[, -1])
 
-X <- matrix(1, 10, 10)
+# Testing data
+letter_test <- read.table("Data/letter-test.txt", header = F, colClasses = "numeric")
+Yt <- letter_test[, 1]
+Xt <- as.matrix(letter_test[, -1])
 
-X[1, 5] <- 5
+# [ToDo] Make sure to add column for an intercept to X and Xt
 
-X[4, 3] <- 3
+vec_1 <- rep(1, dim(X)[1])
+vec_2 <- rep(1, dim(Xt)[1])
 
-y <- c(2, 0, 0, 2, 0, 0, 0, 0, 0, 0)
-ind <- rep(1, 10)
-ind <- ind - as.numeric(y == 2)
-ind
-ind[y == 2]
-sum1 <- sapply(1:10, \(i){function_beta(i, X, y)})
+X <- cbind(vec_1, X)
+Xt <- cbind(vec_2, Xt)
 
-sum1
+# Source the LR function
+source("FunctionsLR.R")
 
-M <- matrix(rnorm(10), 20 ,10)
-M <- t(M) %*% M
-diag(M) <- diag(M) + 1
+# [ToDo] Try the algorithm LRMultiClass with lambda = 1 and 50 iterations. Call the resulting object out, i.e. out <- LRMultiClass(...)
 
-solve(M)
+out <- LRMultiClass(Xt, Yt, X, Y, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
+out
 
-chol2inv(chol(M))
+# The code below will draw pictures of objective function, as well as train/test error over the iterations
+plot(out$objective, type = 'o')
+plot(out$error_train, type = 'o')
+plot(out$error_test, type = 'o')
+
+# Feel free to modify the code above for different lambda/eta/numIter values to see how it affects the convergence as well as train/test errors
+
+# [ToDo] Use microbenchmark to time your code with lambda=1 and 50 iterations. To save time, only apply microbenchmark 5 times.
+
+library("microbenchmark")
+#microbenchmark(LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL), times = 5)
+
+# [ToDo] Report the median time of your code from microbenchmark above in the comments below
+
+# Median time: 2.313614 seconds
